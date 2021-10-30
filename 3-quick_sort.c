2612b6bb3a3458c096cdb34dd1338c02a976dc25
Gemma-Mariae <divinechisom1995@gmail.com>
2021-10-30 01:58:30 -0700

    3-O

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -41,7 +41,7 @@ int lomuto_partition(int *array, size_t size, int left, int right)
 		{
 			if (above < below)
 			{
-				swap_ints(array + below, array + above);
+				swap(array + below, array + above);
 				print_array(array, size);
 			}
 			above++;
@@ -50,7 +50,7 @@ int lomuto_partition(int *array, size_t size, int left, int right)
 
 	if (array[above] > *pivot)
 	{
-		swap_ints(array + above, pivot);
+		swap(array + above, pivot);
 		print_array(array, size);
 	}
 
